4776596c9cd905c8f78a72ec279fa5b259e642f9
vijaya bharathi <bharathi@transcenddigital.com>
2021-07-10 10:02:44 +0530

    folder sturucture done

---
+++ src/Routes/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+  HashRouter,
+  withRouter,
+  Redirect
+} from "react-router-dom";
+
+const Routes = ()=>{
+    return(
+        <div>
+            Routes
+        </div>
+    )
+}
+export default Routes;
